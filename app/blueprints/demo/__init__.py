38fda1ada7d68708757ab72d8b5ac102f3fee492
Shimada666 <649940882@qq.com>
2020-01-18 04:18:08 +0800

    修复bug

--- app/blueprints/demo/__init__.py
+++ app/blueprints/demo/__init__.py
@@ -13,9 +13,7 @@ def create_demo():
     demo = Blueprint('demo', __name__)
 
     from .friend_links import friend_links_rp
-    from .articles import articles_rp
     friend_links_rp.register(demo)
-    articles_rp.register(demo)
 
     return demo
 
@@ -26,13 +24,3 @@ demo_bp = create_demo()
 @demo_bp.route('/main')
 def main_page():
     return common_render('page/main/index.html')
-
-#
-# @demo_bp.route('/articles-list')
-# def articles_list():
-#     return common_render('page/articles/list/index.html')
-
-#
-# @demo_bp.route('/friend-links')
-# def friend_links():
-#     return common_render('page/links/list/index.html')
